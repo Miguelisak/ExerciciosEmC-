23b49348f7efa5e6cc2f584d40ee2f52a745976d
Miguel <163528153+Miguelisak@users.noreply.github.com>
2026-05-12 10:05:52 -0300

    Exercicios feito no HackerRank

---
+++ C++/Exercicio001.cpp
@@ -0,0 +1,18 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a, b, c;
+    
+    cin >> a >> b >> c;
+    
+    int Soma = a + b + c;
+    
+    cout << Soma << endl;  
+    return 0;
+}
